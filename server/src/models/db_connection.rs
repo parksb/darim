ff9665dcb16f4ff1deabf53e217f8ed951145b12
ParkSB <parkgds@gmail.com>
2020-04-05 03:22:12 +0900

    fix: Fix .env file DB_USER variable typo

--- server/src/models/db_connection.rs
+++ server/src/models/db_connection.rs
@@ -4,7 +4,7 @@ use mysql::{Pool, PooledConn, Error};
 pub fn connect() -> Result<PooledConn, Error> {
   dotenv::dotenv().expect("Failed to read .env file");
 
-  let user = env::var("DB_USR").expect("db user not found");
+  let user = env::var("DB_USER").expect("db user not found");
   let password = env::var("DB_PASSWORD").expect("db password not found");
   let host = env::var("DB_HOST").expect("db host not found");
   let name = env::var("DB_NAME").expect("db name not found");
