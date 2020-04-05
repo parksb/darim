fb80a9782433667f036372b8694d1811ea5f4cbe
ParkSB <parkgds@gmail.com>
2020-04-06 01:02:07 +0900

    refactor: Change json stringified hashmap to json macro

--- server/src/routes/post.rs
+++ server/src/routes/post.rs
@@ -1,5 +1,5 @@
 use actix_web::{get, post, delete, patch, Responder, HttpResponse, web};
-use std::collections::HashMap;
+use serde_json::json;
 
 use crate::services::post;
 use crate::models::post::*;
@@ -7,34 +7,30 @@ use crate::models::post::*;
 /// List posts
 #[get("/posts")]
 pub async fn posts() -> impl Responder {
-    let mut response = HashMap::new();
-    response.insert("data", post::get_list().unwrap_or(vec!()));
-    HttpResponse::Ok().json(response)
+    let response = post::get_list().unwrap_or(vec!());
+    HttpResponse::Ok().json(json!({ "data": response }))
 }
 
 /// Create a post
 #[post("/posts")]
 pub async fn create_post(post: web::Json<CreateArgs>) -> impl Responder {
-    let mut response = HashMap::new();
-    response.insert("data", post::create(post.into_inner()).unwrap_or(false));
-    HttpResponse::Ok().json(response)
+   let response = post::create(post.into_inner()).unwrap_or(false);
+    HttpResponse::Ok().json(json!({ "data": response }))
 }
 
 /// Delete a post
 #[delete("/posts/{id}")]
 pub async fn delete_post(id: web::Path<u64>) -> impl Responder {
-    let mut response = HashMap::new();
-    response.insert("data", post::delete(id.into_inner()).unwrap_or(false));
-    HttpResponse::Ok().json(response)
+    let response = post::delete(id.into_inner()).unwrap_or(false);
+    HttpResponse::Ok().json(json!({ "data": response }))
 }
 
 /// Update a post
 #[patch("/posts/{id}")]
 pub async fn update_post(id: web::Path<u64>, post: web::Json<UpdateArgs>) -> impl Responder {
-    let mut response = HashMap::new();
-    response.insert("data", post::update(
+    let response = post::update(
         id.into_inner(),
         post.into_inner(),
-    ).unwrap_or(false));
-    HttpResponse::Ok().json(response)
+    ).unwrap_or(false);
+    HttpResponse::Ok().json(json!({ "data": response }))
 }
