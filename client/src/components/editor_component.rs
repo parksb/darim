4745292f523eccbe252657bec13070168862db75
ParkSB <parkgds@gmail.com>
2020-04-10 23:37:48 +0900

    feat: Implement editor component

---
+++ client/src/components/editor_component.rs
@@ -0,0 +1,27 @@
+use seed::{*, prelude::*};
+use crate::Msg;
+
+pub fn view() -> Node<Msg> {
+    let container_style = style!{
+        St::Margin => "30px 0 30px 0";
+    };
+    let text_field_style = style!{
+        St::FontSize => "16px";
+        St::MaxWidth => "100%";
+        St::MinWidth => "100%";
+        St::Border => "1px #000 solid";
+        St::Padding => 0;
+    };
+    let button_style = style!{
+        St::FontSize => "14px";
+        St::Width => "100%";
+        St::Height => "25px";
+    };
+
+    section![
+        &container_style,
+        input![&text_field_style, attrs!{At::Placeholder => "name..."}, input_ev(Ev::Input, Msg::NewPostAuthor)],
+        textarea![&text_field_style, attrs!{At::Rows => 3, At::Placeholder => "content..."}, input_ev(Ev::Input, Msg::NewPostContent)],
+        button![&button_style, "submit🚀", ev(Ev::Click, |_| Msg::Create)],
+    ]
+}
--- client/src/lib.rs
+++ client/src/lib.rs
@@ -3,9 +3,10 @@ use chrono::NaiveDateTime;
 
 mod components {
     pub mod post_component;
+    pub mod editor_component;
 }
 
-use crate::components::post_component;
+use crate::components::*;
 
 struct Post {
     pub id: u64,
@@ -15,8 +16,14 @@ struct Post {
     pub updated_at: Option<NaiveDateTime>,
 }
 
+struct NewPost {
+    pub author: Option<String>,
+    pub content: Option<String>,
+}
+
 struct Model {
     pub posts: Vec<Post>,
+    pub new_post: NewPost,
 }
 
 impl Default for Model {
@@ -26,15 +33,34 @@ impl Default for Model {
                 Post { id: 0, author: "park".to_string(), content: "Vestibulum orci massa, iaculis cursus mollis malesuada".to_string(), created_at: NaiveDateTime::from_timestamp(1_586_429_335, 0), updated_at: None },
                 Post { id: 1, author: "lee".to_string(), content: "consectetur adipiscing elit".to_string(), created_at: NaiveDateTime::from_timestamp(1_586_429_335, 0), updated_at: None },
                 Post { id: 2, author: "kim".to_string(), content: "Lorem ipsum dolor sit amet".to_string(), created_at: NaiveDateTime::from_timestamp(1_586_429_335, 0), updated_at: Some(NaiveDateTime::from_timestamp(1_586_429_335, 0)) },
-            ]
+            ],
+            new_post: NewPost { author: None, content: None },
         }
     }
 }
 
 #[derive(Clone)]
-enum Msg { }
+pub enum Msg {
+    Create,
+    NewPostAuthor(String),
+    NewPostContent(String),
+}
 
-fn update(msg: Msg, model: &mut Model, _: &mut impl Orders<Msg>) { }
+fn update(msg: Msg, model: &mut Model, _: &mut impl Orders<Msg>) {
+    match msg {
+        Msg::Create => {
+            if let Some(author) = model.new_post.author.clone() {
+                if let Some(content) = model.new_post.content.clone() {
+                    if !author.is_empty() && !content.is_empty() {
+                        // create a new post
+                    }
+                }
+            }
+        },
+        Msg::NewPostAuthor(author) => model.new_post.author = Some(author),
+        Msg::NewPostContent(content) => model.new_post.content = Some(content),
+    }
+}
 
 fn view(model: &Model) -> impl View<Msg> {
     let wrapper_container_style = style!{
@@ -45,6 +71,7 @@ fn view(model: &Model) -> impl View<Msg> {
     div![
         &wrapper_container_style,
         section![h1!["Patic"]],
+        section![editor_component::view()],
         section![
             model.posts.iter().map(|post| {
                 post_component::view(&post.author, &post.content, post.created_at, post.updated_at)
