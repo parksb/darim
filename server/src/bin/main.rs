3d31e63502e6ba36245596ad756c8b08b4da4c41
ParkSB <parkgds@gmail.com>
2020-04-11 02:27:04 +0900

    feat: Allow CORS local client

--- server/src/bin/main.rs
+++ server/src/bin/main.rs
@@ -1,4 +1,5 @@
 use actix_web::{get, App, HttpServer, Responder, HttpResponse};
+use actix_cors::Cors;
 use std::collections::HashMap;
 
 use patic::routes;
@@ -17,6 +18,11 @@ async fn main() -> std::io::Result<()> {
     let address = "127.0.0.1:8080";
 
     HttpServer::new(|| App::new()
+        .wrap(
+            Cors::new()
+                .allowed_origin("http://localhost:8000")
+                .finish()
+        )
         .service(health_check)
         .service(routes::post::posts)
         .service(routes::post::create_post)
