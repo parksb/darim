f68fad7ce8f18cdcbca2a1603e72fddd677eb2f8
ParkSB <parkgds@gmail.com>
2020-05-16 15:12:14 +0900

    feat: Check user is logged-in at session check util

--- server/src/utils/session_util.rs
+++ server/src/utils/session_util.rs
@@ -1,10 +1,16 @@
 use actix_session::Session;
 
-pub fn set_session(session: Session, user_email: &String, user_name: &String) -> bool {
+pub fn set_session(
+    session: Session,
+    user_id: &u64,
+    user_email: &String,
+    user_name: &String,
+) -> bool {
+    let is_set_user_id = session.set("user_id", user_id);
     let is_set_user_email = session.set("user_email", user_email);
     let is_set_user_name = session.set("user_name", user_name);
 
-    return if is_set_user_email.is_err() || is_set_user_name.is_err() {
+    return if is_set_user_id.is_err() || is_set_user_email.is_err() || is_set_user_name.is_err() {
         false
     } else {
         true
@@ -15,14 +21,29 @@ pub fn unset_session(session: Session) {
     session.clear();
 }
 
-pub fn check_session(session: &Session) -> bool {
-    let user_email = session.get::<String>("user_email");
-    let user_name = session.get::<String>("user_name");
+pub fn is_logged_in_user(session: &Session, user_id: Option<&u64>) -> bool {
+    let user_id_in_session = session.get::<u64>("user_id");
+    let user_email_in_session = session.get::<String>("user_email");
+    let user_name_in_session = session.get::<String>("user_name");
 
-    if user_email.is_err() || user_name.is_err() {
+    if user_id_in_session.is_err()
+        || user_email_in_session.is_err()
+        || user_name_in_session.is_err()
+    {
         false
-    } else if user_email.unwrap().is_some() && user_name.unwrap().is_some() {
-        true
+    } else if (&user_id_in_session).as_ref().unwrap().is_some()
+        && user_email_in_session.unwrap().is_some()
+        && user_name_in_session.unwrap().is_some()
+    {
+        if user_id.is_some() {
+            if &user_id_in_session.unwrap().unwrap() == user_id.unwrap() {
+                true
+            } else {
+                false
+            }
+        } else {
+            true
+        }
     } else {
         false
     }
