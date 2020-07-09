8c8800bf7cecfe4351d1d08553504f77b8417d3f
ParkSB <parkgds@gmail.com>
2020-07-10 01:29:05 +0900

    test: Improve password util test

--- server/src/utils/password_util.rs
+++ server/src/utils/password_util.rs
@@ -1,3 +1,4 @@
+use cfg_if::cfg_if;
 use scrypt::{scrypt_check, scrypt_simple, ScryptParams};
 
 /// Returns a password that is hashed by scrypt.
@@ -8,7 +9,7 @@ use scrypt::{scrypt_check, scrypt_simple, ScryptParams};
 ///
 /// # Example
 ///
-/// ```
+/// ```ignore
 /// use scrypt::scrypt_check;
 /// use darim::utils::password_util::get_hashed_password;
 ///
@@ -18,8 +19,20 @@ use scrypt::{scrypt_check, scrypt_simple, ScryptParams};
 /// assert!(scrypt_check(&password, &hashed_password).is_ok());
 /// ```
 pub fn get_hashed_password(password: &str) -> String {
-    let params = ScryptParams::new(15, 8, 1).unwrap();
+    let params = get_params_for_password_hashing();
     scrypt_simple(password, &params).unwrap()
+}
+
+cfg_if! {
+    if #[cfg(test)] {
+        fn get_params_for_password_hashing() -> ScryptParams {
+            ScryptParams::new(7, 4, 1).unwrap()
+        }
+    } else {
+        fn get_params_for_password_hashing() -> ScryptParams {
+            ScryptParams::new(15, 8, 1).unwrap()
+        }
+    }
 }
 
 /// Compares a plain-text password between hashed password
@@ -31,16 +44,40 @@ pub fn get_hashed_password(password: &str) -> String {
 ///
 /// # Example
 ///
-/// ```
+/// ```ignore
 /// use scrypt::{ScryptParams, scrypt_simple};
 /// use darim::utils::password_util::check_password;
 ///
 /// let password = String::from("123");
-/// let params = ScryptParams::new(12, 8, 1).unwrap();
+/// let params = ScryptParams::new(7, 4, 1).unwrap();
 /// let hashed_password = scrypt_simple(&password, &params).unwrap();
 ///
 /// assert!(check_password(&password, &hashed_password));
 /// ```
 pub fn check_password(password: &str, hashed_password: &str) -> bool {
     scrypt_check(password, hashed_password).is_ok()
+}
+
+#[cfg(test)]
+mod tests {
+    use scrypt::{scrypt_check, scrypt_simple, ScryptParams};
+
+    use super::*;
+
+    #[test]
+    fn test_get_hashed_password() {
+        let password = String::from("123");
+        let hashed_password = get_hashed_password(&password);
+
+        assert!(scrypt_check(&password, &hashed_password).is_ok());
+    }
+
+    #[test]
+    fn test_check_password() {
+        let password = String::from("123");
+        let params = ScryptParams::new(7, 4, 1).unwrap();
+        let hashed_password = scrypt_simple(&password, &params).unwrap();
+
+        assert!(check_password(&password, &hashed_password));
+    }
 }
