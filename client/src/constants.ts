a10026537e6858d940101ea68c417d4805cf8b85
ParkSB <parkgds@gmail.com>
2020-09-20 22:55:54 +0900

    refactor: Change constants to environment variables

--- client/src/constants.ts
+++ client/src/constants.ts
@@ -1,2 +1,2 @@
-export const serverBaseUrl = 'http://localhost:8080';
-export const localStoragePrivateKey = 'key';
+export const serverBaseUrl = process.env.SERVER_BASE_URL || 'http://localhost:8080';
+export const localStoragePrivateKey = process.env.LOCAL_STORAGE_PRIVATE_KEY || 'key';
