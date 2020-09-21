caa713c8b990ff22e2b1f01d9ff88eab77cf2ed8
parksb <parkgds@gmail.com>
2020-09-21 23:52:24 +0900

    feat: Support TLS protocol

--- client/src/constants.ts
+++ client/src/constants.ts
@@ -1,2 +1,2 @@
-export const serverBaseUrl = process.env.SERVER_BASE_URL || 'http://localhost:8080';
+export const serverBaseUrl = process.env.SERVER_BASE_URL || 'https://localhost:9600';
 export const localStoragePrivateKey = process.env.LOCAL_STORAGE_PRIVATE_KEY || 'key';
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,11 +1,15 @@
 use actix_cors::Cors;
 use actix_session::CookieSession;
 use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use rustls::internal::pemfile::{certs, rsa_private_keys};
+use rustls::{NoClientAuth, ServerConfig};
 use std::collections::HashMap;
 use std::env;
+use std::fs::File;
+use std::io::BufReader;
 
-use chrono::Duration;
 use darim::routes;
+use time::Duration;
 
 /// Health check
 #[get("/")]
@@ -15,12 +19,21 @@ async fn health_check() -> impl Responder {
     HttpResponse::Ok().json(response)
 }
 
-#[actix_rt::main]
+#[actix_web::main]
 async fn main() -> std::io::Result<()> {
     dotenv::dotenv().expect("Failed to read .env file");
     let address = env::var("ADDRESS").expect("ADDRESS not found");
+    let cert_file_path = env::var("TLS_CERT_FILE_PATH").expect("TLS_CERT_FILE_PATH not found");
+    let key_file_path = env::var("TLS_KEY_FILE_PATH").expect("TLS_KEY_FILE_PATH not found");
 
-    HttpServer::new(move || {
+    let mut config = ServerConfig::new(NoClientAuth::new());
+    let cert_file = &mut BufReader::new(File::open(cert_file_path).unwrap());
+    let key_file = &mut BufReader::new(File::open(key_file_path).unwrap());
+    let cert_chain = certs(cert_file).unwrap();
+    let mut keys = rsa_private_keys(key_file).unwrap();
+    config.set_single_cert(cert_chain, keys.remove(0)).unwrap();
+
+    HttpServer::new(|| {
         App::new()
             .wrap(Cors::new().supports_credentials().finish())
             .wrap(
@@ -34,7 +47,7 @@ async fn main() -> std::io::Result<()> {
             .configure(routes::user::init_routes)
             .configure(routes::auth::init_routes)
     })
-    .bind(address)?
+    .bind_rustls(address, config)?
     .run()
     .await
 }
