a4f1012fe38c20803ecaaf16584a9144adce561c
parksb <parkgds@gmail.com>
2020-09-22 22:47:04 +0900

    refactor: Change smtp transport to sendmail

--- server/src/utils/email_util.rs
+++ server/src/utils/email_util.rs
@@ -1,14 +1,11 @@
-use lettre::{Message, SmtpTransport, Transport};
+use lettre::{Message, SendmailTransport, Transport};
 use std::env;
 
 use crate::models::error::ServiceError;
 
 pub fn send_email(to: &str, subject: &str, body: &str) -> Result<bool, ServiceError> {
     let email_address = env::var("EMAIL_ADDRESS").expect("EMAIL_ADDRESS not found");
-    let smtp_relay_address = env::var("SMTP_RELAY_ADDRESS").expect("SMTP_RELAY_ADDRESS not found");
-
     let parsed_email_address = email_address.parse().unwrap();
-
     let email = Message::builder()
         .from(parsed_email_address)
         .to(to.parse().unwrap())
@@ -16,9 +13,8 @@ pub fn send_email(to: &str, subject: &str, body: &str) -> Result<bool, ServiceEr
         .body(body)
         .unwrap();
 
-    let mailer = SmtpTransport::relay(&smtp_relay_address).unwrap().build();
-
-    match mailer.send(&email) {
+    let sender = SendmailTransport::new();
+    match sender.send(&email) {
         Ok(_) => Ok(true),
         Err(_) => Err(ServiceError::EmailFailure(to.to_string())),
     }
