413e36bdc02ea3921a0b55a7e60449ff33d87370
parksb <parkgds@gmail.com>
2020-10-09 00:44:57 +0900

    refactor: Remove TLS certificate from server

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,10 +1,6 @@
 use actix_web::{get, App, HttpResponse, HttpServer, Responder};
-use rustls::internal::pemfile::{certs, rsa_private_keys};
-use rustls::{NoClientAuth, ServerConfig};
 use std::collections::HashMap;
 use std::env;
-use std::fs::File;
-use std::io::BufReader;
 
 use darim::routes;
 
@@ -20,15 +16,6 @@ async fn health_check() -> impl Responder {
 async fn main() -> std::io::Result<()> {
     dotenv::dotenv().expect("Failed to read .env file");
     let address = env::var("ADDRESS").expect("ADDRESS not found");
-    let cert_file_path = env::var("TLS_CERT_FILE_PATH").expect("TLS_CERT_FILE_PATH not found");
-    let key_file_path = env::var("TLS_KEY_FILE_PATH").expect("TLS_KEY_FILE_PATH not found");
-
-    let mut config = ServerConfig::new(NoClientAuth::new());
-    let cert_file = &mut BufReader::new(File::open(cert_file_path).unwrap());
-    let key_file = &mut BufReader::new(File::open(key_file_path).unwrap());
-    let cert_chain = certs(cert_file).unwrap();
-    let mut keys = rsa_private_keys(key_file).unwrap();
-    config.set_single_cert(cert_chain, keys.remove(0)).unwrap();
 
     println!("Server running at {}", address);
 
@@ -39,7 +26,7 @@ async fn main() -> std::io::Result<()> {
             .configure(routes::user::init_routes)
             .configure(routes::auth::init_routes)
     })
-    .bind_rustls(address, config)?
+    .bind(address)?
     .run()
     .await
 }
