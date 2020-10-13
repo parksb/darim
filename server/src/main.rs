efd3f84a3730276d12b57b17e3cd683c7152f04d
parksb <parkgds@gmail.com>
2020-10-14 01:59:22 +0900

    refactor: Move module declarations to main

--- server/src/main.rs
+++ server/src/main.rs
@@ -2,7 +2,60 @@ use actix_web::{get, App, HttpResponse, HttpServer, Responder};
 use std::collections::HashMap;
 use std::env;
 
-use darim::routes;
+#[macro_use]
+mod macros;
+
+#[macro_use]
+extern crate diesel;
+
+/// A data layer that can access the database and define data structures.
+pub mod models {
+    /// Model related to authentication.
+    pub mod auth;
+    /// Model related to Database connection.
+    pub mod connection;
+    /// Model related to error.
+    pub mod error;
+    /// Model related to post.
+    pub mod post;
+    /// Model related to user.
+    pub mod user;
+    /// Model related to user key.
+    pub mod user_key;
+}
+
+/// A presentation layer that makes API public and passes request/response data to other layers.
+pub mod routes {
+    /// API related to authentication.
+    pub mod auth;
+    /// API related to post.
+    pub mod post;
+    /// API related to user.
+    pub mod user;
+}
+
+/// A business layer that processes the transaction.
+pub mod services {
+    /// Service related to authentication.
+    pub mod auth;
+    /// Service related to post.
+    pub mod post;
+    /// Service related to user.
+    pub mod user;
+}
+
+/// Reusable functions for multiple modules.
+pub mod utils {
+    /// Utilities related to email.
+    pub mod email_util;
+    /// Utilities related to HTTP.
+    pub mod http_util;
+    /// Utilities related to password.
+    pub mod password_util;
+}
+
+/// A database schema.
+pub mod schema;
 
 /// Health check
 #[get("/")]
