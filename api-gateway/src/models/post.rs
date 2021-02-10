021b51b3aa69ac01865b5fd5e8088b513610b3d1
parksb <parkgds@gmail.com>
2021-02-10 17:29:31 +0900

    fix: Remove inheritance from reference

--- api-gateway/src/models/post.rs
+++ api-gateway/src/models/post.rs
@@ -2,7 +2,7 @@ use chrono::NaiveDateTime;
 use serde::{Deserialize, Serialize};
 
 /// Arguments for `POST /posts` API.
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize)]
 pub struct CreateArgs {
     pub title: String,
     pub content: String,
@@ -12,8 +12,10 @@ pub struct CreateArgs {
 /// Arguments for `POST /posts` API of the service.
 #[derive(Serialize, Deserialize)]
 pub struct ServiceCreateArgs {
-    pub create_args: CreateArgs,
     pub user_id: u64,
+    pub title: String,
+    pub content: String,
+    pub date: NaiveDateTime,
 }
 
 /// Arguments for `PATCH /posts/:id` API.
@@ -27,8 +29,10 @@ pub struct UpdateArgs {
 /// Arguments for `PATCH /posts/:id` API of the service.
 #[derive(Serialize, Deserialize)]
 pub struct ServiceUpdateArgs {
-    pub update_args: UpdateArgs,
     pub user_id: u64,
+    pub title: Option<String>,
+    pub content: Option<String>,
+    pub date: Option<NaiveDateTime>,
 }
 
 /// Post DTO using between api gateway and the service.
