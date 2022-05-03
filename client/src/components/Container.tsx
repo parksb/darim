88e008186929b39b8d4919418e9ccf966bac300f
parksb <parkgds@gmail.com>
2022-05-03 22:00:49 +0900

    feat: extend max width of containers

--- client/src/components/Container.tsx
+++ client/src/components/Container.tsx
@@ -11,7 +11,7 @@ export const Container = styled(Section)<ContainerProps>`
   width: 100%;
   flex: ${(props) => !props.row && props.fullHeight && 1};
   height: ${(props) => (props.fullHeight ? '100%' : 'auto')};
-  max-width: ${(props) => (props.fullWidth ? 'none' : '800px')};
+  max-width: ${(props) => (props.fullWidth ? 'none' : '900px')};
   margin-top: ${(props) => (props.top ? `${props.top}px` : 0)};
   margin-bottom: ${(props) => (props.bottom ? `${props.bottom}px` : 0)};
   margin-right: auto;
